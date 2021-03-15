8f85e9f7d71f9489629f04776bae6ef30525646e
Kovalev <test@gmail.ru>
2021-03-15 17:36:41 +0300

    add sql methods commit

--- src/Cat.java
+++ src/Cat.java
@@ -9,8 +9,8 @@ public class Cat implements SQLObject{
     int age;
     double weight;
 
-    public Cat(int id, String name, int type_id, int age, double weight) {
-        this.id = id;
+    public Cat(String name, int type_id, int age, double weight) {
+        this.id = -1;
         this.name = name;
         this.type_id = type_id;
         this.age = age;
@@ -29,6 +29,16 @@ public class Cat implements SQLObject{
         }
     }
 
+    public Cat create_random_cat(){
+        int types_count = 61;
+        int name_i = (int) (Math.random() * Const.names.length);
+        int type_id = (int) (Math.random() * types_count);
+        int age = (int) (Math.random() * 20);
+        double weight =  Math.round(Math.random() * 3000) / 100.0;
+        String name = Const.names[name_i];
+        return new Cat(name, type_id, age, weight);
+    }
+
     @Override
     public String toString() {
         return "Cat{" +
@@ -42,7 +52,15 @@ public class Cat implements SQLObject{
 
     @Override
     public void add_to_DB(Statement stmt) {
-
+        String query = "INSERT INTO 'cats' ('name', 'type_id', 'age', weight)"+
+                "VALUES ('"+name+"', '"+type_id+"', '"+
+                age+"', '"+weight+"')";
+        //System.out.println(query);
+        try {
+            stmt.execute(query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete_from_DB(Statement stmt) {
@@ -51,6 +69,15 @@ public class Cat implements SQLObject{
 
     @Override
     public void update_in_DB(Statement stmt) {
-
+        String query = "UPDATE 'cats' "+
+                "SET name=" + name + ", type_id=" + type_id + ", "+
+                "age=" + age + ", weight=" + weight + " " +
+                "WHERE id=" + id;
+        //System.out.println(query);
+        try {
+            stmt.execute(query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
